a4eee32619eab054f5076a94abf9fc7858dd6be6
Poga Po <poga.bahamut@gmail.com>
2017-01-10 04:25:09 +0800

    start

---
+++ README.md
@@ -0,0 +1,16 @@
+# dat-get
+
+wget for dat
+
+`npm i -g dat-get`
+
+## Usage
+
+```
+# download test.js to cwd
+$ dat-get dat://419eb9467cb7240297640864fd4eeb8c3bb0ea209e8c43563613b078a236f0ab/test.js
+```
+
+## License
+
+The MIT License
---
+++ index.js
@@ -0,0 +1,25 @@
+const hyperdrive = require('hyperdrive')
+const memdb = require('memdb')
+const swarm = require('hyperdiscovery')
+const URL = require('url')
+const pump = require('pump')
+const fs = require('fs')
+const path = require('path')
+
+const url = URL.parse(process.argv[2])
+
+pump(open(url), fs.createWriteStream(path.basename(url.pathname)), err => {
+  if (err) console.log(err)
+
+  process.exit()
+})
+
+function open (url) {
+  if (url.protocol !== 'dat:') throw new Error('only works on dat:// url')
+
+  var drive = hyperdrive(memdb())
+  var archive = drive.createArchive(url.host, {sparse: true})
+  swarm(archive)
+
+  return archive.createFileReadStream(url.pathname.replace(/^\//, ''))
+}
